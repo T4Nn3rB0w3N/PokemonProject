03dcba3a0d04540dc0456d05a8bc4192803435a9
Bowen <tbow1382@740s-pl-120.local>
2019-02-04 09:19:18 -0700

    PokedexPanel is now set.
    It's NOT completed, but it has all of the main parts that it needs: data members, constructor, panel set-up, layout set-up, and listener set-up.

--- src/pokemon/view/PokedexPanel.java
+++ src/pokemon/view/PokedexPanel.java
@@ -1,6 +1,43 @@
 package pokemon.view;
 
-public class PokedexPanel
+import javax.swing.*;
+import pokemon.controller.PokedexController;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class PokedexPanel extends JPanel
 {
+	private PokedexController appController;
+	private JButton updateButton;
+	private SpringLayout appLayout;
+	
+	public PokedexPanel(PokedexController appController)
+	{
+		super();
+		this.appController = appController;
+		
+		appLayout = new SpringLayout();
+		updateButton = new JButton("UPDATE");
+		
+		setupPanel();
+		setupLayout();
+		setupListeners();	
+	}
+	
+	private void setupPanel()
+	{
+		this.setLayout(appLayout);
+		this.add(updateButton);
+	}
+	
+	private void setupLayout()
+	{
+		
+	}
+	
+	private void setupListeners()
+	{
+		
+	}
 
 }
